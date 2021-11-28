103a1897252839164aa3619489a7c934429dc908
aliffurqon <aqonlimited@gmail.com>
2021-11-29 04:20:28 +0700

    penambahan dependecy cors & pengubahan port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+const cors = require("cors");
 
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
